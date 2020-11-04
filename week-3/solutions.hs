4474c3060618a5c86dcd8b8350a29ddce93904ba
Iliya Zhecev <iliya.zhechev@gmail.com>
2020-11-04 14:38:55 +0200

    Add last solution to week 3

--- week-3/solutions.hs
+++ week-3/solutions.hs
@@ -28,3 +28,19 @@ collatz 1 = [1]
 collatz n = n : collatz m
   where
     m = if even n then n `div` 2 else 3 * n + 1
+
+type Function a = a -> a
+
+derive :: Fractional a => a -> (a -> a) -> a -> a
+derive eps f x = (f (x + eps) - f x) / eps
+
+df :: (Function Double) -> Double -> Double
+df = derive 1e-10
+
+newton'sMethod :: (Eq t, Num t) => t -> Double -> (Function Double) -> Double
+newton'sMethod its x0 f = loop its x0
+  where
+    nextStep xn = xn - (f xn / df f xn)
+
+    loop 0 x = x
+    loop its x = loop (its - 1) $ nextStep x
